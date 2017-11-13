6ec0ea1fdf9c93496cdbcccf191cf7a53e9ad94b
phadrian <pheh.adrian@gmail.com>
2017-11-13 14:56:37 +0800

    fix

--- mm-sm.cu
+++ mm-sm.cu
@@ -217,6 +217,7 @@ __global__ void mm_kernel(matrix a, matrix b, matrix result, int size)
         __syncthreads();
     }
 
+    printf("%f ", resultValue);
     setElement(subResult, threadRow, threadCol, resultValue);
 }
 
